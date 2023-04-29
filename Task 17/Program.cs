0c08a84c7249ee5d7953eef6c69276e6ed9760e0
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-04-29 13:34:03 +0300

    Решение задачи 17

---
+++ Task 17/Program.cs
@@ -0,0 +1,18 @@
+﻿// 17. Программа, которая которая определчет сколько чисел из M заданных больше 0.
+
+int EnterInfomation(string message)
+{
+Console.Write(message);
+return int.Parse(Console.ReadLine()!);
+}
+int LengthArray = EnterInfomation(" Введите количество чисел : ");
+int [] N=new int [LengthArray];
+int Count=0;
+for (int i=0;i<N.Length;i++)
+{
+System.Console.Write($"Введите число {i + 1}: ");
+int M=int.Parse(Console.ReadLine()!);
+if (M > 0)
+Count++;
+}
+System.Console.Write($": всего {N.Length} чисел, количество чисел больше 0 = {Count}");
